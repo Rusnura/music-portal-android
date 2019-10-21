a54215a7d49e57aeb1c3169f2f424d1cc9266d9a
Ruslan Tumasov <Rus-nura@yandex.ru>
2019-10-21 21:12:41 +0700

    Released RegisterPage Activity and LoginPageActivity improved

--- app/src/main/java/org/tumasov/rmusicplayer/LoginActivity.java
+++ app/src/main/java/org/tumasov/rmusicplayer/LoginActivity.java
@@ -1,13 +1,21 @@
 package org.tumasov.rmusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button signUpButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signUpButton = findViewById(R.id.signUp_button);
+        loginButton = findViewById(R.id.login_button);
+        signUpButton.setOnClickListener((listener) -> startActivity(new Intent(this, SignUpActivity.class)));
     }
 }
--- app/src/main/java/org/tumasov/rmusicplayer/SignUpActivity.java
+++ app/src/main/java/org/tumasov/rmusicplayer/SignUpActivity.java
@@ -2,13 +2,22 @@ package org.tumasov.rmusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+    private Button backButton;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        backButton = findViewById(R.id.back_button);
+        registerButton = findViewById(R.id.doSignUp_button);
+
+        backButton.setOnClickListener((l) -> startActivity(new Intent(this, LoginActivity.class)));
     }
 }
