3c5f3b93a70ebffda7dc5f20dcda80dcb6992d4d
Ruslan Tumasov <Rus-nura@yandex.ru>
2019-12-08 22:25:50 +0700

    Move player logic from Activity to PlayerFragment

--- app/src/main/java/org/tumasov/rmusicplayer/ui/SongsActivity.java
+++ app/src/main/java/org/tumasov/rmusicplayer/ui/SongsActivity.java
@@ -1,19 +1,12 @@
 package org.tumasov.rmusicplayer.ui;
 
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
 import android.util.Log;
-import android.view.View;
-import android.widget.SeekBar;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,55 +16,29 @@ import org.tumasov.rmusicplayer.helpers.JSONUtils;
 import org.tumasov.rmusicplayer.helpers.adapters.SongAdapter;
 import org.tumasov.rmusicplayer.helpers.api.ServerAPI;
 import org.tumasov.rmusicplayer.helpers.http.entities.HttpResponse;
-import org.tumasov.rmusicplayer.helpers.player.PlayerMessages;
-import org.tumasov.rmusicplayer.services.AudioService;
-import org.tumasov.rmusicplayer.services.AudioServiceBinder;
-import java.io.IOException;
+import org.tumasov.rmusicplayer.ui.fragments.PlayerFragment;
 import java.util.LinkedList;
 import java.util.List;
 
 public class SongsActivity extends FragmentActivity {
     private ServerAPI serverAPI = ServerAPI.getInstance();
-    private View footerRelativeLayout;
-    private SeekBar audioPositionBar;
+    private PlayerFragment playerFragment;
     private String selectedPlaylistId;
     private SongAdapter songAdapter;
-    private AudioServiceBinder audioServiceBinder = null;
-    private Handler audioProgressUpdateHandler;
-
-    private ServiceConnection serviceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-            audioServiceBinder = (AudioServiceBinder) iBinder;
-            audioServiceBinder.getPlayer().setAudioProgressUpdateHandler(audioProgressUpdateHandler);
-            if (audioServiceBinder.getPlayer().getPlaylist() == null) { // Set playlist to MP3Player
-                audioServiceBinder.getPlayer().setPlaylist(songAdapter.getSongs());
-            }
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName componentName) {}
-    };
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
 
-        footerRelativeLayout = findViewById(R.id.audio_list_footer);
-        audioPositionBar = findViewById(R.id.audioPosition);
+        fragmentManager = getSupportFragmentManager();
+        playerFragment = (PlayerFragment) fragmentManager.findFragmentById(R.id.audio_list_footer);
         selectedPlaylistId = getIntent().getStringExtra("playlistId");
         RecyclerView songsRecyclerView = findViewById(R.id.songs_list);
         songsRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         songAdapter = new SongAdapter((song, index) -> {
-            try {
-                audioServiceBinder.getPlayer().play(getApplicationContext(), index);
-                if (footerRelativeLayout.getVisibility() != View.VISIBLE) {
-                    footerRelativeLayout.setVisibility(View.VISIBLE);
-                }
-            } catch (IOException e) {
-                Log.e("SongsActivity", "Cannot to start MP3 Player");
-            }
+            playerFragment.play(index);
         });
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
@@ -85,36 +52,6 @@ public class SongsActivity extends FragmentActivity {
                 serverAPI.getSongsFromPlaylist(selectedPlaylistId, this::handleRequest);
             }
         }
-
-        audioProgressUpdateHandler = new Handler(message -> {
-            if (audioServiceBinder != null) {
-                if (message.what == PlayerMessages.UPDATE_AUDIO_PROGRESS_BAR) {
-                    int currentProgress = audioServiceBinder.getPlayer().getCurrentAudioPosition();
-                    int totalProgress = audioServiceBinder.getPlayer().getTotalAudioDuration();
-                    if (totalProgress != audioPositionBar.getMax()) {
-                        audioPositionBar.setMax(totalProgress);
-                    }
-                    audioPositionBar.setProgress(currentProgress);
-                }
-                return true;
-            }
-            return false;
-        });
-
-        audioPositionBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (fromUser && audioServiceBinder != null) {
-                    audioServiceBinder.getPlayer().setProgress(progress);
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
     }
 
     private void handleRequest(HttpResponse response) {
@@ -141,41 +78,7 @@ public class SongsActivity extends FragmentActivity {
         });
     }
 
-    private void bindAudioService() {
-        if (audioServiceBinder == null) {
-            Intent intent = new Intent(SongsActivity.this, AudioService.class);
-
-            // Below code will invoke serviceConnection's onServiceConnected method.
-            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
-            Log.i("SongsActivity", "bindAudioService successfully!");
-        }
-        Log.i("SongsActivity", "bindAudioService ended!");
-    }
-
-    private void unBoundAudioService() {
-        if (audioServiceBinder != null) {
-            unbindService(serviceConnection);
-            audioServiceBinder = null;
-            Log.i("SongsActivity", "unBoundAudioService successfully!");
-        }
-        Log.i("SongsActivity", "unBoundAudioService ended!");
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        bindAudioService();
-    }
-
-    @Override
-    public void onPause() {
-        unBoundAudioService();
-        super.onPause();
-    }
-
-    @Override
-    protected void onDestroy() {
-        unBoundAudioService();
-        super.onDestroy();
+    public SongAdapter getSongAdapter() {
+        return this.songAdapter;
     }
 }
--- app/src/main/java/org/tumasov/rmusicplayer/ui/fragments/PlayerFragment.java
+++ app/src/main/java/org/tumasov/rmusicplayer/ui/fragments/PlayerFragment.java
@@ -1,18 +1,129 @@
 package org.tumasov.rmusicplayer.ui.fragments;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import org.tumasov.rmusicplayer.R;
+import org.tumasov.rmusicplayer.helpers.player.PlayerMessages;
+import org.tumasov.rmusicplayer.services.AudioService;
+import org.tumasov.rmusicplayer.services.AudioServiceBinder;
+import org.tumasov.rmusicplayer.ui.SongsActivity;
+import java.io.IOException;
 
 public class PlayerFragment extends Fragment {
+    private AudioServiceBinder audioServiceBinder = null;
+    private Handler audioProgressUpdateHandler;
+    private SeekBar audioPositionBar;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            audioServiceBinder = (AudioServiceBinder) iBinder;
+            audioServiceBinder.getPlayer().setAudioProgressUpdateHandler(audioProgressUpdateHandler);
+            audioServiceBinder.getPlayer().setPlaylist(((SongsActivity)getActivity()).getSongAdapter().getSongs());
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {}
+    };
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_player, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        audioProgressUpdateHandler = new Handler(message -> {
+            if (audioServiceBinder != null) {
+                if (message.what == PlayerMessages.UPDATE_AUDIO_PROGRESS_BAR) {
+                    int currentProgress = audioServiceBinder.getPlayer().getCurrentAudioPosition();
+                    int totalProgress = audioServiceBinder.getPlayer().getTotalAudioDuration();
+                    if (totalProgress != audioPositionBar.getMax()) {
+                        audioPositionBar.setMax(totalProgress);
+                    }
+                    audioPositionBar.setProgress(currentProgress);
+                }
+                return true;
+            }
+            return false;
+        });
+
+        audioPositionBar = view.findViewById(R.id.audioPosition);
+        audioPositionBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser && audioServiceBinder != null) {
+                    audioServiceBinder.getPlayer().setProgress(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        bindAudioService();
+    }
+
+    @Override
+    public void onPause() {
+        unBoundAudioService();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        unBoundAudioService();
+        super.onDestroy();
+    }
+
+    private void bindAudioService() {
+        if (audioServiceBinder == null) {
+            Intent intent = new Intent(getActivity(), AudioService.class);
+            getActivity().bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+            Log.i("SongsActivity", "bindAudioService successfully!");
+        }
+        Log.i("SongsActivity", "bindAudioService ended!");
+    }
+
+    private void unBoundAudioService() {
+        if (audioServiceBinder != null) {
+            getActivity().unbindService(serviceConnection);
+            audioServiceBinder = null;
+            Log.i("SongsActivity", "unBoundAudioService successfully!");
+        }
+        Log.i("SongsActivity", "unBoundAudioService ended!");
+    }
+
+    public boolean play(int index) {
+        if (audioServiceBinder != null) {
+            try {
+                audioServiceBinder.getPlayer().play(getContext(), index);
+                return true;
+            } catch (IOException e) {
+                return false;
+            }
+        }
+        return false;
     }
 }
