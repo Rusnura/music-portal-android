c1f1e4016f6618a381ad0b7ea0e19e87fa83ca22
Ruslan Tumasov <Rus-nura@yandex.ru>
2019-12-19 21:31:41 +0700

    Fixed notification for Android.O

--- app/src/main/java/org/tumasov/rmusicplayer/services/AudioService.java
+++ app/src/main/java/org/tumasov/rmusicplayer/services/AudioService.java
@@ -10,25 +10,23 @@ import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
-
-import androidx.annotation.RequiresApi;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
 import org.tumasov.rmusicplayer.R;
 import org.tumasov.rmusicplayer.helpers.player.MP3Player;
 import org.tumasov.rmusicplayer.ui.SongsActivity;
+import java.util.Objects;
 
 public class AudioService extends Service {
     private static final String NOTIFICATION_ID = "17122019";
     private final MP3Player player = MP3Player.getInstance();
     private final IBinder audioServiceBinder = new AudioServiceBinder();
+    private Notification notification;
 
-//    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onCreate() {
         super.onCreate();
-        createNotificationChannel();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel();
+        }
 
         player.setOnMP3PlayerPrepareCompleteListener(p -> {
             Intent songsActivityIntent = new Intent(this, SongsActivity.class);
@@ -36,41 +34,26 @@ public class AudioService extends Service {
             PendingIntent pendInt = PendingIntent.getActivity(this, 0, songsActivityIntent, 0);
             Notification.Builder builder = new Notification.Builder(this);
             builder.setContentIntent(pendInt)
-                    .setSmallIcon(R.drawable.play)
-                    .setTicker(p.getPlayingSong().getTitle())
-                    .setOngoing(true)
-//                    .setChannelId(NOTIFICATION_ID)
-                    .setContentTitle(p.getPlayingSong().getArtist() + " - " + p.getPlayingSong().getTitle())
-                    .setContentText(p.getPlayingSong().getTitle());
-            Notification notification = builder.build();
-            startForeground(Integer.parseInt(NOTIFICATION_ID), notification);
+                .setSmallIcon(R.drawable.play)
+                .setTicker(p.getPlayingSong().getTitle())
+                .setOngoing(true)
+                .setContentTitle(p.getPlayingSong().getArtist() + " - " + p.getPlayingSong().getTitle())
+                .setContentText(p.getPlayingSong().getTitle());
 
-//            Notification notification = new NotificationCompat.Builder(this, NOTIFICATION_ID)
-//                    .setSmallIcon(R.drawable.play)
-//                    .setContentTitle(p.getPlayingSong().getTitle())
-//                    .setContentText("Song")
-//                    .setOngoing(true)
-//                    .addAction(R.drawable.previous_24dp, "Previous", playbackAction(3))
-//                    .addAction(plaorpa, "Pause", playbackAction(1))
-//                    .addAction(R.drawable.next_24dp, "Next", playbackAction(2))
-//                    .setContentIntent(contentIntent)
-//                    .setStyle(new android.support.v4.media.app.NotificationCompat.MediaStyle()
-//                            .setShowActionsInCompactView(0, 1, 2)
-//                            .setMediaSession(mediaSession.getSessionToken()))
-//                    .setSubText(artist)
-//                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-//                    .build();
-//            notificationManager.notify(Integer.parseInt(NOTIFICATION_ID), notification);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                builder.setChannelId(NOTIFICATION_ID);
+            }
+            notification = builder.build();
+            startForeground(Integer.parseInt(NOTIFICATION_ID), notification);
         });
     }
 
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_ID,
-                    "Song", NotificationManager.IMPORTANCE_DEFAULT);
-            notificationChannel.setDescription("HEY");
+                    "RMusicPlayer", NotificationManager.IMPORTANCE_DEFAULT);
             NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(notificationChannel);
+            Objects.requireNonNull(manager).createNotificationChannel(notificationChannel);
         }
     }
 
